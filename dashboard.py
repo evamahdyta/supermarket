e0105df40429b28da2288b0414d896f81a3ccd31
eva <evamahdyta@gmail.com>
2025-02-04 16:58:11 +0700

    update2

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
+
 
 # Load dataset
 def load_data():
